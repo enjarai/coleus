8b43fb56e9f8f8ed8c1d6b2be45266cb7ee9219a
Master-Bw3 <bw1jochman@gmail.com>
2025-07-01 23:29:34 -0500

    mc text to html component

--- src/client/kotlin/mod/master_bw3/coleus/Components.kt
+++ src/client/kotlin/mod/master_bw3/coleus/Components.kt
@@ -3,8 +3,12 @@ package mod.master_bw3.coleus
 import com.glisco.isometricrenders.render.ItemRenderable
 import com.glisco.isometricrenders.render.RenderableDispatcher
 import j2html.TagCreator
+import j2html.tags.ContainerTag
+import j2html.tags.DomContent
+import j2html.tags.Tag
 import j2html.tags.specialized.ImgTag
 import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
 import java.nio.file.Path
 import kotlin.io.path.relativeTo
 
@@ -16,6 +20,47 @@ public object Components {
         imageOutPath.parent.toFile().mkdirs()
         image.writeTo(imageOutPath)
         return TagCreator.img().withSrc(imageOutPath.relativeTo(pagePath.parent).toString())
+    }
+
+    @JvmStatic
+    public fun text(text: Text): DomContent {
+        return constructText(text)
+    }
+
+    private fun constructText(text: Text): Tag<*> {
+        val out = TagCreator.span()
+        var current: ContainerTag<*> = out
+
+        val style = text.style
+        if (style.isBold) {
+            val child = TagCreator.strong()
+            current.with(child)
+            current = child
+        }
+        if (style.isItalic) {
+            val child = TagCreator.em()
+            current.with(child)
+            current = child
+        }
+        if (style.isUnderlined) {
+            current.withClass("underline")
+        }
+        if (style.isObfuscated) {
+            current.withClass("obfuscated")
+        }
+        val color = style.getColor();
+        if (color != null) {
+            current.withStyle("color: " + color.hexCode)
+        }
+
+        println(text.copyContentOnly().string)
+        current.withText(text.copyContentOnly().string)
+
+        for (sibling in text.siblings) {
+            current.with(constructText(sibling))
+        }
+
+        return out
     }
 
 }
